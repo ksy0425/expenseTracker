147e80b167d985d4590e7f1716673e8e541a4e9e
ksy0425 <pko04258@gmail.com>
2026-04-29 11:20:28 +0900

    init: IOS프로그래밍 최종 프로젝트 기본 설정

---
+++ smartExpenseTracker/smartExpenseTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smartExpenseTracker
+//
+//  Created by 김성윤 on 4/29/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
